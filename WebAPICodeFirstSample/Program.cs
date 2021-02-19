14ca5b4f3a6655128dae2a21c311829adcd6b36a
anpt1992 <pham.an.20.09@gmail.com>
2021-02-19 17:17:07 +0700

    disable https

--- WebAPICodeFirstSample/Program.cs
+++ WebAPICodeFirstSample/Program.cs
@@ -20,6 +20,7 @@ namespace WebAPICodeFirstSample
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://localhost:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
